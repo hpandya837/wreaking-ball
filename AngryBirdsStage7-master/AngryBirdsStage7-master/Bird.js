6579ae0f3169e12a13f7b30d9ff5e5bf054ad50b
hpandya837 <66964248+hpandya837@users.noreply.github.com>
2020-10-11 12:54:36 -0400

    Add files via upload

---
+++ AngryBirdsStage7-master/AngryBirdsStage7-master/Bird.js
@@ -0,0 +1,23 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("ball2.png");
+   // this.smokeImage = loadImage("sprites/smoke.png");
+   // this.trajectory =[];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+   // if(this.body.velocity.x > 10 && this.body.position.x > 200){
+     /// var position = [this.body.position.x, this.body.position.y];
+      //this.trajectory.push(position);
+   // }
+   
+
+   
+  }
+}
